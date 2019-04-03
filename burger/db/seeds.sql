ad876bc4035802955b6307932c3b355b0c02daad
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-02 17:05:51 -0700

    files & code

---
+++ burger/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tasty McGee', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Portobello Mushroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cardiac Arrest', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Delight', true);
