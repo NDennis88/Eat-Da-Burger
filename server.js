de2c7120ef5f2eb7a75881efadc3a6789bff6484
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-02 17:38:25 -0700

    server.js template

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.use(express.static("public"));
+
+
